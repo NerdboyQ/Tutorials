53cb91d5d96b0e6a13aa0c73d4c128e311f68011
NerdboyQ <princtonb@gmail.com>
2024-09-27 16:04:24 -0700

    api script pulling full history successfully

--- DataVisualization/3DPrint_Sales/cults3d_api_pull.py
+++ DataVisualization/3DPrint_Sales/cults3d_api_pull.py
@@ -1,6 +1,7 @@
 import json
 import pprint
 import requests
+import pandas as pd
 from requests.auth import HTTPBasicAuth
 from private_config import *
 
@@ -26,7 +27,7 @@ graphql_query = """
 graphql_query = """
 {
     myself {
-    salesBatch(limit:100){
+    salesBatch(limit:100 offset:){
       total
       results{
         createdAt
@@ -64,6 +65,48 @@ pyld = "query="+graphql_query
 headers = {
     'Content-Type': 'application/x-www-form-urlencoded',
 }
-r = requests.post("https://cults3d.com/graphql", headers=headers, auth=credentials, data=pyld)
-j = json.loads(r.text)
-pprint.pprint(j)
+
+class SALE:
+  cost: float
+  country: str
+  countryCode: str
+  design: str
+  saleDate: str
+  user: str
+
+def json_to_df(jobj: json) -> None:
+  sales = []
+  for r in jobj:
+    # print("\t-",r)
+    sale = SALE()
+    sale.cost=r['income']['cents']
+    sale.country=r['orderCountry']['name']
+    sale.countryCode=r['orderCountry']['code']
+    sale.design=r['creation']['name']
+    sale.saleDate=r['createdAt']
+    sale.user=r['user']['nick']
+    sales.append(sale.__dict__)
+    print("\t-",sales[-1])
+  
+  return sales
+
+sales = []
+# Per Cults3D CTO, their graphQL api is limited to
+# 100 results, but if looped w/ an offset to 100
+# the full list of results can be rettrieved. The 
+# loop can end when no more results are found
+offset = 0
+while True:
+  r = requests.post("https://cults3d.com/graphql", headers=headers, auth=credentials, data=pyld.replace("offset:",f"offset:{offset}"))
+  # print(r)
+  j = json.loads(r.text)
+  jResults = j['data']['myself']['salesBatch']['results']
+  if not len(jResults): break
+  sales+=json_to_df(jResults)
+  offset+=100
+  # pprint.pprint(j)
+
+df = pd.DataFrame(sales)
+df.to_csv("cults3D_sales.csv", index=False)
+# print(df)
+print("done")
